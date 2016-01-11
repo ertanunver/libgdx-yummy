361b3ef17a90410ff7538d8f76dcb9377ec4372e
Ertan Ünver <ertanunver@gmail.com>
2016-01-11 13:37:53 +0200

    LoadingStage for testing

--- core/src/com/ertanunver/libgdx/yummy/test/LoadingStage.java
+++ core/src/com/ertanunver/libgdx/yummy/test/LoadingStage.java
@@ -1,9 +1,10 @@
 package com.ertanunver.libgdx.yummy.test;
 
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.ertanunver.libgdx.yummy.framework.YMAssetManager;
 import com.ertanunver.libgdx.yummy.framework.YMStage;
 import com.ertanunver.libgdx.yummy.framework.YMStageManager;
-import com.google.gson.JsonObject;
+import com.ertanunver.libgdx.yummy.framework.YMTestStage;
 
 /**
  * Created by ertanunver on 10/01/16.
@@ -12,6 +13,25 @@ public class LoadingStage extends YMStage {
 
     public LoadingStage() {
         super(new ScreenViewport());
+    }
+
+    @Override
+    public void render() {
+        System.out.println("LoadingStage.render");
+        if (YMAssetManager.getInstance().update()) {
+            YMStageManager.getInstance().pushStageWithKillingActiveStage(new YMTestStage("Second Stage"));
+        }
+        super.render();
+    }
+
+    @Override
+    public void create() {
+        System.out.println("LoadingStage.create");
+    }
+
+    @Override
+    public void dispose() {
+        System.out.println("LoadingStage.dispose");
     }
 
 }
