759597452d12b8737af1478ed9b68d7c587e5f10
Ertan Ünver <ertanunver@gmail.com>
2016-01-11 13:37:28 +0200

    YMTestStage class

---
+++ core/src/com/ertanunver/libgdx/yummy/framework/YMTestStage.java
@@ -0,0 +1,27 @@
+package com.ertanunver.libgdx.yummy.framework;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.ertanunver.libgdx.yummy.framework.YMSkinManager;
+import com.ertanunver.libgdx.yummy.framework.YMStage;
+
+/**
+ * Created by ertanunver on 11/01/16.
+ */
+public class YMTestStage extends YMStage {
+
+    private String message;
+    private Label vLabel;
+
+    public YMTestStage(String message) {
+        super(new ScreenViewport());
+        this.message = message;
+    }
+
+    @Override
+    public void create() {
+        vLabel = new Label(message, YMSkinManager.getInstance().getSkin());
+        addActor(vLabel);
+    }
+
+}
