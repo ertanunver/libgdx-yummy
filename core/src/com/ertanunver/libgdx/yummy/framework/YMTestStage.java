c5196068a59cde428c33ac0516bef04445ccb216
Ertan Ünver <ertanunver@gmail.com>
2016-02-12 00:12:30 +0200

    no message

--- core/src/com/ertanunver/libgdx/yummy/framework/YMTestStage.java
+++ core/src/com/ertanunver/libgdx/yummy/framework/YMTestStage.java
@@ -1,7 +1,10 @@
 package com.ertanunver.libgdx.yummy.framework;
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
